ab38b9b7957e0b477469766692384a0837326784
imad-oi <oissafe.imad@gmail.com>
2023-11-27 19:51:48 +0100

    add framer motion

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,35 @@
+
+"use client"
+
+import fetchAnime from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { AnimeProp } from "./AnimeCard";
+
+
+let page = 2 ;
 
 function LoadMore() {
+  const { ref, inView } = useInView();  // We use the useInView hook to detect when the spinner is in the viewport.
+  const [data , setData] = useState<AnimeProp[]>([]) ; // We use the useState hook to store the data we get from the API.
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]); // We use the spread operator to add the new data to the existing data.
+        page++;
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
+     <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+       {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
